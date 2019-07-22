e0bfdc9d3dd58ab1922b22608f21b388c76212af
kkarolak <kkarolak@netkodo.com>
2019-07-22 14:16:35 +0200

    Authenticate user by JWT

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  post 'auth/register', to: 'users#register'
+  post 'auth/login', to: 'users#login'
+  get 'test', to: 'users#test'
   resources :articles do
     resources :comments
   end
